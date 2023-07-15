f8e547fa209f79e1a9dc1ed4f57804b8c9e45f6a
inugmanov <nugmanov.i.j@gmail.com>
2023-07-15 16:46:13 +0500

    add result text

--- BMI/bmi.py
+++ BMI/bmi.py
@@ -18,6 +18,8 @@ class App(ctk.CTk):
     self.columnconfigure(0, weight = 1)
     self.rowconfigure((0,1,2,3), weight = 1, uniform = 'a')
 
+    ResultText(self)
+
     self.mainloop()
 
   def change_title_bar_color(self):
@@ -28,6 +30,12 @@ class App(ctk.CTk):
       windll.dwmapi.DwmSetWindowAttribute(HWND, DWMWA_ATTRIBUTE, byref(c_int(COLOR)), sizeof(c_int))
     except:
       pass
+
+class ResultText(ctk.CTkLabel):
+  def __init__(self, parent):
+    font = ctk.CTkFont(family = FONT, size = MAIN_TEXT_SIZE, weight = 'bold')
+    super().__init__(master = parent, text = 22.5, font = font, text_color = WHITE)
+    self.grid(column = 0, row = 0, rowspan = 2, sticky = 'nsew')
 
 if __name__ == '__main__':
   App()
