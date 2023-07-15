d38ca8b74e6f7ccefb5519bc747df95d49a1fed7
inugmanov <nugmanov.i.j@gmail.com>
2023-07-15 12:24:53 +0500

    add new frame

--- qr-code/main.py
+++ qr-code/main.py
@@ -8,12 +8,23 @@ class App(ctk.CTk):
     ctk.set_appearance_mode('light')
     super().__init__(fg_color = 'white')
 
-    #customize
     self.title('')
     self.iconbitmap('empty.ico')
     self.geometry('400x400')
 
-    #run app
+    #Entry field
+    EntryField(self)
+
     self.mainloop()
+
+class EntryField(ctk.CTkFrame):
+  def __init__(self, parent):
+    super().__init__(master = parent, corner_radius = 20, fg_color = '#021FB3')
+    self.place(relx = 0.5, rely = 1, relwidth = 1, relheight = 0.4, anchor = 'center')
+
+    self.rowconfigure((0,1), weight = 1, uniform = 'a')
+    self.columnconfigure(0, weight = 1, uniform = 'a')
+
+
 
 App()
